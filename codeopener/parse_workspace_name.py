afea3fdf0d2ce1f2e020c18112f40aa7ebadfa5b
KTachibanaM <whj19931115@gmail.com>
2024-01-25 23:31:16 -0800

    fix parse workspace name that does not return space-replaced names

--- codeopener/parse_workspace_name.py
+++ codeopener/parse_workspace_name.py
@@ -7,4 +7,4 @@ def parse_workspace_name(folder_uri: str) -> str:
         raise CodeOpenerException(f"Encountered an invalid folder_uri: {folder_uri}")
     workspace_name = splits[-1]
     workspace_name = workspace_name.replace("%20", " ")
-    return splits[-1]
+    return workspace_name
