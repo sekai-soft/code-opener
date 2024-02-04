d6413827f63bce8359694ce5270581688e4712c6
KTachibanaM <whj19931115@gmail.com>
2024-02-03 17:19:30 -0800

    open shortcuts folder

--- tray.py
+++ tray.py
@@ -8,6 +8,7 @@ from infi.systray import SysTrayIcon
 from codeopener.loop import loop
 from codeopener.windows_native_notification import windows_native_notification
 from codeopener.logger import log_file
+from codeopener.ensure_start_menu_folder import ensure_start_menu_folder
 
 loop_interval = 10
 
@@ -16,11 +17,16 @@ def _loop():
             windows_native_notification(loop())
             time.sleep(loop_interval)
 
-
 def open_logs(_):
     subprocess.run(["PowerShell", "Get-Content", "-Path", log_file, "-Wait"])
 
-menu_options = (("Logs", None, open_logs),)
+def open_shortcuts_folder(_):
+    subprocess.run(["explorer", ensure_start_menu_folder()])
+
+menu_options = (
+    ("Logs", None, open_logs),
+    ("Open shortcuts folder", None, open_shortcuts_folder),
+)
 
 
 def main():
